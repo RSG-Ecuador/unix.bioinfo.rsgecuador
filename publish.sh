06b93251cd0499a01718b98074573d405ef8bf81
jezur <juanesteban.zurita@gmail.com>
2021-05-24 12:18:44 -0500

    “actualización”

--- publish.sh
+++ publish.sh
@@ -3,4 +3,10 @@ cp -r _files _build/html/
 cp README_LIBRO.md _build/html/
 cp Linux_BashAwk_scripting_BootCampt_RSG_iGEM_Ecuador_portada.png _build/html/
 mv ./_build/html/README_LIBRO.md ./_build/html/README.md
-ghp-import -n -p -f _build/html
+cd ./_build/html/
+git init -b gh-pages
+git remote add origin https://github.com/RSG-Ecuador/unix.bioinfo.rsgecuador
+git status
+git add -A
+git commit -m “actualización”
+git push origin gh-pages
